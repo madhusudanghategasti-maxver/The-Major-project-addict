dbb0f66c6beb2ce4429ee9065c4b961616c3104f
DEMONDEXTER <madhusudanghategasti@gmail.com>
2026-08-04 17:56:53 +0530

    updating the ML model

--- app.py
+++ app.py
@@ -1,48 +1,20 @@
 import pickle
-from flask import Flask, request, jsonify, render_template, url_for  # type: ignore
-
+from flask import Flask, request, jsonify, render_template, url_for 
 
 app = Flask(__name__)
-model = pickle.load(open('DTCmodel.pkl', 'rb'))
-std = pickle.load(open('scaling.pkl','rb'))
+model = pickle.load(open('RFC_model.pkl','rb'))
+gender = pickle.load(open('gender.pkl','rb'))
+stress = pickle.load(open('stress.pkl','rb')) 
+academic = pickle.load(open('academic.pkl','rb')) 
+
 
 @app.route('/')
-def home():
-    return render_template('home.html')
+def loadPage():
+    return render_template('loadingscreen/loading.html')
 
-@app.route('/sign-up')
+@app.route('/signup')
 def signup():
-    return render_template("sign-up.html")
-
-@app.route("/prediction")
-def form():
-    return render_template("prediction.html")
-
-
-
-@app.route("/predict_api",methods=['POST'])
-def predict_api():
-    data = request.json['data']
-    print(data)
-    print(np.array(list(data.values())).reshape(1,-1))
-    new_data = std.transform(np.array(list(data.values())).reshape(1,-1))
-    output = model.predict(new_data)
-    print(output[0])
-    return jsonify(output[0])
-
-@app.route("/predict",methods=['POST'])
-def predict():
-    data = [float(x) for x in request.form.values()]
-    final_input = std.transform(np.array(data).reshape(1,-1))
-    print(final_input)
-    output = model.predict(final_input)[0]
-    if output == 0:
-        output = "NO"
-    else:
-        output = "YES"
-    return render_template("form.html",prediction_test='your prediction is {}'.format(output))
-
-
+    return render_template('sign_up/sign_up.html')
 
 if __name__ == "__main__":
     app.run(debug=True)
